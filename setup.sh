459ab13ec47842726d1f550807a3ab57985616b7
di2nk <79467462+di2nk@users.noreply.github.com>
2021-06-16 06:48:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,20 +25,10 @@ echo "Enter the VPS Subdomain Hostname, if not available, please click Enter"
 read -p "Hostname / Domain: " host
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 wget https://netzvps.herokuapp.com/vpn/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://netzvps.herokuapp.com/vpn/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-wget https://netzvps.herokuapp.com/vpn/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
-wget https://netzvps.herokuapp.com/vpn/ssr.sh && chmod +x ssr.sh
-wget https://netzvps.herokuapp.com/vpn/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 wget https://netzvps.herokuapp.com/vpn/ins-vt.sh && chmod +x ins-vt.sh && sed -i -e 's/\r$//' ins-vt.sh && screen -S v2ray ./ins-vt.sh
-wget https://netzvps.herokuapp.com/vpn/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 rm -f /root/ssh-vpn.sh
-rm -f /root/sstp.sh
-rm -f /root/wg.sh
-rm -f /root/sodosok.sh
-rm -f /root/ssr.sh
 rm -f /root/ins-vt.sh
 rm -f /root/go.sh
-rm -f /root/ipsec.sh
 history -c
 echo "1.1" > /home/ver
 clear
