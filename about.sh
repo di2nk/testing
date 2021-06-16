1f1df86c1dbc2bc42051b57dff4b638bea4106d2
di2nk <79467462+di2nk@users.noreply.github.com>
2021-06-16 07:00:30 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install SSH & OpenVPN             #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & 10 Buster 64 bit               #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by Adi Subagja                       #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Adi Subagja                                   #"
+echo -e "# Allah                                         #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# Owner VPS                                     #"
+echo -e "# VPNSTORES.NET                                 #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
